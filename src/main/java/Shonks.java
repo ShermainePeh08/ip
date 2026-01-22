b6bd68f33926c3f1f20ae934dd390420761c5386
Shermaine <pehshermaine@gmail.com>
2026-01-22 23:48:12 +0800

    Level-2: Add, List

--- src/main/java/Shonks.java
+++ src/main/java/Shonks.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Shonks {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> items = new ArrayList<>();
 
         System.out.println("Hello! I'm Shonks");
         System.out.println("What can I do for you?");
@@ -14,6 +17,16 @@ public class Shonks {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
+
+            if (input.equals("list")) {
+                for (int i = 0; i < items.size(); i++) {
+                    System.out.println((i + 1) + ". " + items.get(i));
+                }
+                continue;
+            }
+
+            items.add(input);
+            System.out.println("added: " + input);
 
             System.out.println(input);
         }
