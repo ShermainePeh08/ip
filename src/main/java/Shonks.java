11e427c67219a502a42e10be91b37383b80b78a9
Shermaine <pehshermaine@gmail.com>
2026-01-22 23:27:41 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Shonks.java
@@ -0,0 +1,7 @@
+public class Shonks {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Shonks");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
