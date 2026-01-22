ad9f10eee2c604e5ae83498b72dcfb1b372ce09d
Shermaine <pehshermaine@gmail.com>
2026-01-22 23:41:42 +0800

    Level 1: Echo

--- src/main/java/Shonks.java
+++ src/main/java/Shonks.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Shonks {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Shonks");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(input);
+        }
     }
 }
+
